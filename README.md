fba19cbdbc5cb12023397b14d260807ed1d0085d
Lucas de Assis <100141186+Lucassis7@users.noreply.github.com>
2024-01-04 19:21:08 +0000

    fix: format problems

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tabnews-repo
+
 Repository of studies with the creation of an open source application like Tabnews.
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 function Home() {
-    return (
-        <h1>Hello World! We're here soon.</h1>
-    );
+  return <h1>Hello World! We're here soon.</h1>;
 }
 
 export default Home;
